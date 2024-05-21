6848ab0152dfbe8164eef13ac4bef59d4ae97c15
Gavin Popken <gavinpopkenart@gmail.com>
2024-05-20 22:30:46 -0500

    continuing to play with the endpoints, looking to send over a CSRF token

--- backend/backend/api/urls.py
+++ backend/backend/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('session/', views.add_to_sess, name='add_to_sess'),
-    path('test_get/', views.test_get, name='test_get')
+    path('session/', views.add_to_sess, name='add_to_sess'),  #POST
+    path('test_get/', views.test_get, name='test_get'),  #GET
 ]
--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# POST, works with Postman, not with React yet
 def add_to_sess(request):
     print('You are adding to the session, supposedly.', request.body)
     return HttpResponse(status=200)
 
+# GET
 def test_get(request):
     print('You are GETTING')
     return HttpResponse('You have gotten some text.')
