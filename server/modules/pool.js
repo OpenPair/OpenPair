b9397a9060365507094702a9e6f69d87526151ae
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2024-05-23 16:45:24 -0500

    Changed the databse name

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -21,7 +21,7 @@ else {
     pool = new pg.Pool({
         host: 'localhost',
         port: 5432,
-        database: '8bitcoffee',   // TODO:	💥 Change this to the name of your database!
+        database: 'open_pair',   // TODO:	💥 Change this to the name of your database!
     });
 }
 
