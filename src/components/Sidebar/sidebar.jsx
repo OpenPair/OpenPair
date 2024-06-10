c15a3369f682b590bf86204b44f6c644fa9084e3
Marcos Andrade <mnandrade1999@gmail.com>
2024-06-10 12:56:13 -0500

    simple Offcanvas implementation for base, controlled with button

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,11 +1,39 @@
-import './sidebar.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react'
+import { Button } from 'reactstrap'
+import { Offcanvas, OffcanvasBody, OffcanvasHeader } from 'reactstrap'
+
+import '../styles/main.scss'
+
 export default function Sidebar(){
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return(
-    <>
-      <div class="container">
-    <h1>Hey There Square</h1>
-      </div>
-    </>
+
+    <div>
+      <Button
+	color="primary"
+	onClick={toggle}
+      >
+	Open
+      </Button>
+      <Offcanvas isOpen={isOpen}
+      toggle={toggle}>
+	<OffcanvasHeader >
+	  Offcanvas
+	</OffcanvasHeader>
+	<OffcanvasBody>
+	  <strong>
+            This is the Offcanvas body.
+	  </strong>
+	</OffcanvasBody>
+      </Offcanvas>
+    </div>
+    
   )
 }
+
+
