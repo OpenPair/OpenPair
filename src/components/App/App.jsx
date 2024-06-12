1b88b072f167114842991d2bc82e0efbd6bba88b
Marcos Andrade <mnandrade1999@gmail.com>
2024-06-11 21:19:20 -0500

    Reorganized/cleaned app.jsx and commented unneeded passport stuff

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,14 +10,14 @@ import {
 
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css'
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute.jsx';
-import LoginPage from '../LoginPage/LoginPage.jsx';
-import RegisterPage from '../RegisterPage/RegisterPage.jsx';
-import UserPage from '../UserPage/UserPage.jsx';
 import AboutPage from '../AboutPage/About.jsx'
 import ContactPage from '../ContactPage/Contact.jsx'
+/* import ProtectedRoute from '../ProtectedRoute/ProtectedRoute.jsx';
+* import LoginPage from '../LoginPage/LoginPage.jsx';
+* import RegisterPage from '../RegisterPage/RegisterPage.jsx';
+* import UserPage from '../UserPage/UserPage.jsx'; */
 
-function App() {
+export default function App() {
   const dispatch = useDispatch();
 
   const user = useSelector(store => store.user);
@@ -25,7 +25,6 @@ function App() {
   useEffect(() => {
     dispatch({ type: 'FETCH_USER' });
   }, [dispatch]);
-
 
 
   return (
@@ -40,9 +39,7 @@ function App() {
     </nav>
     
       <div>
-        {/* <Nav /> */}
         <Routes>
-          {/* Visiting localhost:5173 will redirect to localhost:5173/home */}
 	  <Route
 	    path="/about"
 	    element={<AboutPage />}
@@ -52,11 +49,38 @@ function App() {
             path="/"
             element={<Navigate replace to="/home"/>}
           />
+
+          <Route
+            exact path="/home"
+            element=
+            {user.id ?
+              // If the user is already logged in, 
+              // redirect them to the /user page
+              redirect("/user")
+            :
+	     <h1>Heyyyyy there square</h1>
+            }
+          />
+          <Route
+            path="/contact"
+            element={<ContactPage />}
+          />
+
+          {/* If none of the other routes matched, we will show a 404. */}
+          <Route
+            path="*"
+            element={<h1>404 - Not Found</h1>}
+          />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
           {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:5173/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:5173/user */}
-          <Route
+				  {/* <Route
             exact path="/user"
             element={
               <ProtectedRoute>
@@ -89,33 +113,5 @@ function App() {
               <RegisterPage />
             }
           />
+ */}
 
-          <Route
-            exact path="/home"
-            element=
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              redirect("/user")
-            :
-	     <h1>Heyyyyy there square</h1>
-            }
-          />
-          <Route
-            path="/contact"
-            element={<ContactPage />}
-          />
-
-          {/* If none of the other routes matched, we will show a 404. */}
-          <Route
-            path="*"
-            element={<h1>404 - Not Found</h1>}
-          />
-        </Routes>
-    
-      </div>
-    </Router>
-  )
-}
-
-export default App
