f2f110bce28e8fffb7ef13e8a6bdf65421478187
Marcos Andrade <mnandrade1999@gmail.com>
2024-06-21 16:07:27 -0500

    start of refactoring data for sidebar

---
+++ src/components/Sidebar/sidebarData.jsx
@@ -0,0 +1,37 @@
+import { BsHouseDoor } from "react-icons/bs";
+export const SidebarData = [
+    {
+        title: "Home",
+        path: "/",
+        icon: <BsHouseDoor />,
+
+    },
+
+    // {
+    //     title: "About",
+    //     path: "/about",
+    //     icon: <IoIcons.IoIosPaper />,
+    //     iconClosed: <RiIcons.RiArrowDownSFill />,
+    //     iconOpened: <RiIcons.RiArrowUpSFill />,
+	
+    // },
+    // {
+    //     title: "Contact",
+    //     path: "/contact",
+    //     icon: <FaIcons.FaPhone />,
+    // },
+    // {
+    //     title: "Settings",
+    //     path: "/settings",
+    //     icon: <FaIcons.FaEnvelopeOpenText />,
+
+    //     iconClosed: <RiIcons.RiArrowDownSFill />,
+    //     iconOpened: <RiIcons.RiArrowUpSFill />,
+
+    // },
+    // {
+    //     title: "Help",
+    //     path: "/help",
+    //     icon: <IoIcons.IoMdHelpCircle />,
+    // },
+];
