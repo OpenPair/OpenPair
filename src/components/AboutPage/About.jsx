1f42d651322fb05d281c0c98ee96b862f3dda0f2
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2024-09-02 18:55:01 -0500

    Moved the vitestartup stuff over to the homepage and added some text into the about page

--- src/components/AboutPage/About.jsx
+++ src/components/AboutPage/About.jsx
@@ -1,7 +1,12 @@
 export default function AboutPage(){
 
   return(
-    <h1>HEy there square</h1>
+    <div>
+    <h1>About the project</h1>
+    <p>OpenPair is a project that allows you to ask questions about your code and get answers from the community.</p>
+    <p>It is built with React, Node.js, Express, and MongoDB.</p>
+    <p>It is a work in progress and is not yet ready for production.</p>
+    </div>
 
     
   )
